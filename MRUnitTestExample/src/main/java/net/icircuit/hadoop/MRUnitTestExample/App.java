e580f1312bdec3ed7d3ce3959e17a6f5998b7267
Sankar Cheppali <Sankar Cheppali>
2016-09-10 11:29:23 +0530

    added MRUnit example

---
+++ MRUnitTestExample/src/main/java/net/icircuit/hadoop/MRUnitTestExample/App.java
@@ -0,0 +1,13 @@
+package net.icircuit.hadoop.MRUnitTestExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
